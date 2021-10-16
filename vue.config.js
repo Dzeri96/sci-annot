a7387e046214b1e7c7959b323d67005ebfed7269
Filip Darmanovic <e1527089@student.tuwien.ac.at>
2021-10-16 21:57:57 +0200

    Added HtmlWebpackInlineSourcePlugin - Works

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 // The app version is available in the code
 process.env.VUE_APP_VERSION = process.env.npm_package_version;
 
 module.exports = {
   lintOnSave: false,
+  css: {
+    extract: false,
+  },
   configureWebpack: {
     devtool: 'source-map',
     optimization: {
       minimize: true
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: 'output.html', // the output file name that will be created
+        template: 'public/index.html', // this is important - a template file to use for insertion
+        inlineSource: '.(js|css)$' // embed all javascript and css inline
+      }),
+      new HtmlWebpackInlineSourcePlugin()
+    ]
   }
 }
